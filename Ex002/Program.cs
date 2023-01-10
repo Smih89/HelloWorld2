56610f86fa382ddf4843bd18a7c67cb54c222aef
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-01-10 12:52:44 +0300

    Знакомство

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Writeline("Введите ваше имя ");
 string username = Console.ReadLine();
-Console.WriteLine("Привет Привет Привет, ");
+Console.WriteLine("Привет , ");
 Console.WriteLine(username);
